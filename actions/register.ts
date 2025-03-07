772b0d729b0686e8b97dc326d114f3c7eabd5a73
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-03-08 01:59:40 +0530

    registerApi

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,7 @@
 "use server";
 import * as z from "zod";
-
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 export const register = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -8,6 +9,30 @@ export const register = async (values: z.infer<typeof LoginSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
+
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //Todo: send veirfication token email
 
   return { success: " Email sent!" };
 };
