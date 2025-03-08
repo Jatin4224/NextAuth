1b0d7b33fa010bbf21c0c41a8020a016dc39fb3d
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-03-09 03:55:41 +0530

    .

--- auth.ts
+++ auth.ts
@@ -1,18 +1,38 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "ADMIN" | "USER";
+};
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
-
+      if (token.role && session.user) {
+        session.user.role = token.role as "ADMIN" | "USER";
+      }
       return session;
     },
 
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
       return token;
     },
   },
