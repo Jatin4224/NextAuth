e4f979b535555286e36e33c0c1503d9d6124f0a2
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-03-08 21:41:29 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -10,12 +10,31 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   // req.auth
   const { nextUrl } = req;
+
   const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix); //no need to protect this routes
 
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  //order of this matters becasue authRoutes are public routes
+  //make sure u will visit api/auth/providers route by clicking it
+  if (isApiAuthRoute) {
+    return null; //do not do any aciton regarding this
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl)); // u have to pass nextUrl because it create complete local host url
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("auth/login", nextUrl));
+  }
+  return null; //means allow this dont do anything
 });
 
 // Optionally, don't invoke Middleware on some paths
