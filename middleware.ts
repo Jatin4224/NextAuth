da6595b6ee448a9d41e7b0a1da219c125bfb2bf3
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-03-08 05:50:06 +0530

    .

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,21 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "./routes";
 const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   // req.auth
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE", req.nextUrl.pathname);
-  console.log("Is LoggedIn:", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix); //no need to protect this routes
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 });
 
 // Optionally, don't invoke Middleware on some paths
